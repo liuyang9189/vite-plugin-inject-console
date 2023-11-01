057d916c011f903ef15021f5e20b5eadf2cc8c90
liuyang9189 <yokoafter90@gmail.com>
2023-11-01 16:45:37 +0800

    update version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-module.exports = (options) => {
+module.exports = (options = {}) => {
     const { packagePath, outParams } = options
     return {
         transformIndexHtml (html) {
